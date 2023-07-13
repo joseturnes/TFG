d8e774f2a2446ba2ea4846d604baab848d85cc5c
joseturnes <jose.turnes@udc.es>
2023-07-13 19:01:09 +0200

    Clear repo

--- frontend/public/stellariumEngine/dist/js/chunk-2d2253ec.80d31482.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp"]=this["webpackJsonp"]||[]).push([["chunk-2d2253ec"],{e429:function(e,s,n){e.exports=n.p+"js/stellarium-web-engine.adeb1606.wasm"}}]);
-//# sourceMappingURL=chunk-2d2253ec.80d31482.js.map
