96e651efad38e751dad1c31c96169c5d1178d76d
joseturnes <jose.turnes@udc.es>
2023-07-19 00:27:36 +0200

    [FRONTEND-IMAGENES] feat: añadida visualización en detalle

--- frontend/src/views/GaleriaImaxes.vue
+++ frontend/src/views/GaleriaImaxes.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container">
-    <h1 class="title">Galería de Imaxes</h1>
+    <h1 class="title">Galería de Imágenes</h1>
+    <div v-if="showModal" class="modal" @click="closeModal">
+      <img :src="selectedImage" class="modal-content" />
+    </div>
     <div class="image-container">
       <div v-for="(imagen, index) in reversedImages" :key="imagen.id" class="image-item">
-        <img :src="arrayToImage(imagen.datos)" alt="imagen" />
+        <img :src="arrayToImage(imagen.datos)" @click="openModal(arrayToImage(imagen.datos))" alt="imagen" />
       </div>
     </div>
   </div>
@@ -16,6 +19,8 @@ export default {
   data() {
     return {
       imagenes: [],
+      showModal: false,
+      selectedImage: null
     };
   },
   computed: {
@@ -38,6 +43,13 @@ export default {
     arrayToImage(bytes) {
       return `data:image/jpeg;base64,${bytes}`;
     },
+    openModal(src) {
+      this.selectedImage = src;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    }
   },
 };
 </script>
@@ -51,8 +63,26 @@ export default {
 
 .title {
   margin-bottom: 20px;
-  font-family: Verdana, Helvetica, sans-serif;
-  color: white;
+}
+
+.modal {
+  display: flex;
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0,0,0,0.6);
+}
+
+.modal-content {
+  margin: auto;
+  display: block;
+  max-width: 80%;
+  max-height: 80%;
 }
 
 .image-container {
@@ -66,5 +96,6 @@ export default {
   width: 100%;
   height: 100%;
   object-fit: cover;
+  cursor: pointer;
 }
 </style>
